67b59464bafad1ccca5e17915ca34f3151e5e66b
Ertugrul Haskan <ertughaskan@gmail.com>
2021-02-15 16:58:06 -0800

    game begins

--- app.js
+++ app.js
@@ -1,1 +1,85 @@
-console.log('asd');
+function getRandom(max, min) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      monsterHealth: 100,
+      playerHealth: 100,
+      roundCounter: 0,
+      healCounter: 3,
+      winner: null,
+    };
+  },
+  watch: {
+    monsterHealth(value) {
+      if (value <= 0) {
+        this.monsterHealth = 0;
+        this.winner =
+          this.monsterHealth === 0 && this.playerHealth === 0
+            ? 'draw'
+            : 'player';
+      }
+    },
+    playerHealth(value) {
+      if (value <= 0) {
+        this.playerHealth = 0;
+        this.winner =
+          this.monsterHealth === 0 && this.playerHealth === 0
+            ? 'draw'
+            : 'monster';
+      }
+      if (value > 100) {
+        this.playerHealth = 100;
+      }
+    },
+    roundCounter(value) {
+      if (value > 3) {
+        this.roundCounter = 0;
+      }
+    },
+  },
+  computed: {
+    monsterBarStyle() {
+      return { width: this.monsterHealth + '%' };
+    },
+    playerBarStyle() {
+      return { width: this.playerHealth + '%' };
+    },
+  },
+  methods: {
+    newGame() {
+      this.monsterHealth = 100;
+      this.playerHealth = 100;
+      this.roundCounter = 0;
+      this.healCounter = 3;
+      this.winner = null;
+    },
+    attack() {
+      if (this.roundCounter > 0) {
+        this.roundCounter++;
+      }
+      playerAttackVal = getRandom(10, 5);
+      this.monsterHealth -= playerAttackVal;
+      monsterAttackVal = getRandom(15, 10);
+      this.playerHealth -= monsterAttackVal;
+    },
+    specialAttack() {
+      this.roundCounter++;
+      playerAttackVal = getRandom(20, 10);
+      this.monsterHealth -= playerAttackVal;
+      monsterAttackVal = getRandom(15, 5);
+      this.playerHealth -= monsterAttackVal;
+    },
+    heal() {
+      this.healCounter--;
+      healVal = getRandom(30, 15);
+      this.playerHealth += healVal;
+      monsterAttackVal = getRandom(15, 5);
+      this.playerHealth -= monsterAttackVal;
+    },
+  },
+});
+
+app.mount('#app');
